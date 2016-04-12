a0392a5a383fa9ab8b27bd27f757f8d0c7d2b31c
duwonders <752046798@qq.com>
2016-04-12 16:14:48 +0800

    dsa

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var prenum = 1;
+setInterval(function(){
+	if(prenum >= 4)
+		prenum = 0;
+	$('#lunbo').children()[prenum].click();
+}, 4000)
+$('#lunbo').on('click',function(e){
+	var temp = e.target;
+	if(!temp.value)
+		return;
+	$('#lunbo').children('*').map(function(d,t){
+		t.style.background="#fff";
+	});
+	temp.style.background = "#00BFFF";
+	run(e.target.value);
+})
+function run(goal){
+	var preTarget = getPreTarget();
+	var target = $(".main-right li:nth-child("+ goal +")");
+	console.log('当前' + goal);
+	console.log('上一个' + prenum);
+	if(prenum - goal < 0){
+		preTarget.css({
+			'transform':'scale(0)', 
+			'left': '-100%'
+		});
+	}else if(prenum - goal > 0){
+		preTarget.css({
+			'transform':'scale(0)', 
+			'left': '100%'
+		});
+	}
+	console.log(target);
+	target.css({
+		'transform' : 'scale(1)',
+		'left' : '0',
+	});
+}
+function getPreTarget(){
+	var target = $(".main-right").children('*').map(function(d, t){
+		if(t.style.transform == 'scale(1)'){
+			prenum = d + 1;
+			return t;
+		}
+	});
+	return target;
+}
